e2c8beb7df2d33ce82efe037ba4af8760abb63dd
moonwald <monaldofunk@yahoo.it>
2025-01-14 04:10:09 +0100

    added index.html

--- rezaverse/src/index.tsx
+++ rezaverse/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+
 // Find the root element
 const rootElement = document.getElementById('root');
 
