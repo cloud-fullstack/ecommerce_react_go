e9254bb551be8ad19e04fad1358f2c7bbd272101
moonwald <monaldofunk@yahoo.it>
2025-01-19 13:29:03 +0100

    added router in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,20 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
 // Find the root element
 const rootElement = document.getElementById('root');
 
 // Check if the root element exists
 if (!rootElement) {
-  throw new Error("Root element with ID p'root' not found in the DOM.");
+  throw new Error("Root element with ID 'root' not found in the DOM.");
 }
 
 // Create the root and render the app
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rezaverse">
       <App />
     </BrowserRouter>
   </React.StrictMode>
