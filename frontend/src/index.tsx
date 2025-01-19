edaeba8686f136a6c6450e637dc4ae36f872d1b8
moonwald <monaldofunk@yahoo.it>
2025-01-19 14:15:36 +0100

    BrowserRouter basename=rezaverse

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// Find the root element
 const rootElement = document.getElementById('root');
-
-// Check if the root element exists
 if (!rootElement) {
   throw new Error("Root element with ID 'root' not found in the DOM.");
 }
 
-// Create the root and render the app
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
