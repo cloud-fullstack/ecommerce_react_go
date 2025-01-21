061601d7cdebea3448f1a511721cfa8783f65ea6
moonwald <monaldofunk@yahoo.it>
2025-01-21 19:22:16 +0100

    adjustement index + gitlab build frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Root element with ID 'root' not found in the DOM.");
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
-const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/rezaverse">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
