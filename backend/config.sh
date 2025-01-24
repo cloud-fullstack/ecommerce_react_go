5f7cf36ebb34a53c4b8509d59c6061ede42e664a
moonwald <monaldofunk@yahoo.it>
2025-01-24 04:04:56 +0100

    di nuovo .env

--- backend/config.sh
+++ backend/config.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-export DB_HOST=${secrets.DB_HOST}
-export DB_PORT=${secrets.DB_PORT}
-export DB_USER=${secrets.DB_USER}
-export DB_PASSWORD=${secrets.DB_PASSWORD}
-export DB_NAME=${secrets.DB_NAME}
-export SPACES_ACCESS_KEY=${secrets.SPACES_ACCESS_KEY}
-export SPACES_SECRET_KEY=${secrets.SPACES_SECRET_KEY}
+export DB_HOST="${DB_HOST}"
+export DB_PORT="${DB_PORT}"
+export DB_USER="${DB_USER}"
+export DB_PASSWORD="${DB_PASSWORD}"
+export DB_NAME="${DB_NAME}"
+export SPACES_ACCESS_KEY="${SPACES_ACCESS_KEY}"
+export SPACES_SECRET_KEY="${SPACES_SECRET_KEY}"
 
-
-
-# Print out values for debugging
+# Debug output
 echo "DB_HOST: $DB_HOST"
 echo "DB_PORT: $DB_PORT"
 echo "DB_USER: $DB_USER"
@@ -19,4 +17,3 @@ echo "DB_NAME: $DB_NAME"
 echo "SPACES_ACCESS_KEY: $SPACES_ACCESS_KEY"
 echo "SPACES_SECRET_KEY: $SPACES_SECRET_KEY"
 echo "STATIC_PATH: $STATIC_PATH"
-echo "FRON
